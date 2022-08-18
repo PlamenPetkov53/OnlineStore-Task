1caedb1da10e6584a62da06209636a3d98481454
Plamen Petkov <plamen_9915@abv.bg>
2022-08-18 14:36:41 +0300

    serializers add viewset added

--- store/api/models.py
+++ store/api/models.py
@@ -7,6 +7,9 @@ class Product(models.Model):
       title = models.CharField(max_length=255)
       price = models.DecimalField(max_digits = 5, decimal_places = 2)
       history = HistoricalRecords()
+      def __str__(self):
+          return self.title
+      
 
 
 class Order(models.Model):
--- store/api/views.py
+++ store/api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status
+from rest_framework.viewsets import ModelViewSet
+
+from api.models import Product, Order
+from .serializers import ProductSerializer, OrderSerializer
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class OrderViewSet(ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
